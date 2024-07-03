dd72e0bb36c59b90015aacedc7e8c6e2b995a3d1
saadi <mail2sdq99@gmail.com>
2024-07-03 14:03:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+function submitSearch() {
+    let searchValue = document.getElementById("countryName").value;
+    window.location.href = `result.html?country=${searchValue}`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const countryNameInput = document.getElementById("countryName");
+    const countryCardsContainer = document.getElementById("countryCards");
+
+    // Function to fetch and display country cards
+    function fetchAndDisplayCountries() {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+            .then(data => {
+                // Clear previous cards
+                countryCardsContainer.innerHTML = '';
+
+                data.forEach(country => {
+                    // Check if the country name matches the input value (case insensitive)
+                    if (country.name.common.toLowerCase().includes(countryNameInput.value.toLowerCase())) {
+                        // Create card element
+                        let card = document.createElement("div");
+                        card.classList.add("col");
+
+                        // Card structure
+                        card.innerHTML = `
+                            <div class="card h-100">
+                                <img src="${country.flags.png}" class="card-img-top" alt="Flag of ${country.name.common}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${country.name.common}</h5>
+                                    <p class="card-text"><strong>Capital:</strong> ${country.capital}</p>
+                                    <p class="card-text"><strong>Region:</strong> ${country.region}</p>
+                                    <p class="card-text"><strong>Population:</strong> ${country.population}</p>
+                                    <a href="${country.maps.googleMaps}" class="btn btn-primary" target="_blank">View on Google Maps</a>
+                                </div>
+                            </div>
+                        `;
+
+                        // Append card to container
+                        countryCardsContainer.appendChild(card);
+                    }
+                });
+            })
+            .catch(err => {
+                console.error("Error fetching data:", err);
+            });
+    }
+
+    // Call fetchAndDisplayCountries initially to load all countries
+    fetchAndDisplayCountries();
+
+    // Event listener for input change (typing)
+    countryNameInput.addEventListener("input", function () {
+        fetchAndDisplayCountries();
+    });
+
+});
---
+++ result.js
@@ -0,0 +1,78 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const params = new URLSearchParams(window.location.search);
+    const countryName = params.get("country");
+
+    let officialName = document.getElementById("officialName");
+    let name = document.getElementById("name");
+    let img = document.getElementById("img");
+    let capital = document.getElementById("capital");
+    let map = document.getElementById("map");
+
+    if (countryName) {
+        fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data && data.length > 0) {
+                    const country = data[0];
+                    officialName.innerText = country.name.official;
+                    name.innerText = country.name.common;
+                    capital.innerText = country.capital ? country.capital[0] : 'N/A';
+                    img.innerHTML = `<img src="${country.flags.png}" alt="Flag of ${country.name.common}" class="img-fluid">`;
+
+                    // Populate additional details
+                    populateDetail("Area", country.area);
+                    populateDetail("Population", country.population);
+                    populateDetail("Region", country.region);
+                    populateDetail("Subregion", country.subregion);
+                    populateDetail("Timezone", country.timezones[0]); 
+
+                     // Use the provided Google Maps URL
+                     const gMapURL = country.maps.googleMaps;
+                     if (gMapURL) {
+                         // Replace goo.gl/maps with www.google.com/maps/embed?pb=
+                         const embedURL = gMapURL.replace("https://goo.gl/maps/", "https://www.google.com/maps/embed?pb=");
+                         map.src = embedURL;
+                     } else {
+                         map.src = ""; // Clear the iframe if no valid URL is provided
+                     }
+
+               
+                } else {
+                    officialName.innerText = "Country not found";
+                    name.innerText = "";
+                    capital.innerText = "";
+                    img.innerHTML = "";
+                    map.src = "";
+                }
+            })
+            .catch(err => {
+                officialName.innerText = "Error fetching data";
+                name.innerText = "";
+                capital.innerText = "";
+                img.innerHTML = "";
+                map.src = "";
+                console.error(err);
+            });
+    } else {
+        officialName.innerText = "No country specified";
+        name.innerText = "";
+        capital.innerText = "";
+        img.innerHTML = "";
+        map.src = "";
+    }
+
+    function populateDetail(label, value) {
+        const detailContainer = document.createElement("div");
+        detailContainer.classList.add("row");
+        detailContainer.innerHTML = `
+            <div class="col-md-6">
+                <h5>${label}:</h5>
+            </div>
+            <div class="col-md-6">
+                <h5 class="text-end">${value}</h5>
+            </div>
+        `;
+        document.querySelector(".card-body").appendChild(detailContainer);
+    }
+});
